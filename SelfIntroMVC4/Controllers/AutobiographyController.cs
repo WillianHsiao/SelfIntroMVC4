e5430ddbee96d96eea54d342cdbc16bff59e1a9c
WillianHsiao <lucien5335@hotmail.com>
2016-07-12 00:57:43 +0800

    自傳、技能、本網站使用之工具Controller

---
+++ SelfIntroMVC4/Controllers/AutobiographyController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SelfIntroMVC4.Controllers
+{
+    public class AutobiographyController : Controller
+    {
+        // GET: Autobiography
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
